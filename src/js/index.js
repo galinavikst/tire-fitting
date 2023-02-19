f146ef9e37cae96077d3e0aa2a488c756b06a8c1
Halyna Stepanenko <105945642+galinavikst@users.noreply.github.com>
2023-02-19 15:58:32 +0100

    feat: add questions section and its css, js

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
+import { addBgColor, addEventToQuestionBtn } from "./questions.js";
 
+addBgColor();
+
+addEventToQuestionBtn();
---
+++ src/js/questions.js
@@ -0,0 +1,26 @@
+const questionsArr = document.querySelectorAll(".question_item");
+
+export function addBgColor() {
+  for (let i = 0; i < questionsArr.length; i++) {
+    if (i % 2 === 0) {
+      questionsArr[i].classList.add("addition_bg_color");
+    }
+  }
+}
+
+function showHideAnswer(el) {
+  const answer = el.parentElement.nextElementSibling;
+  const liBefore = el.parentElement.parentElement;
+
+  el.classList.toggle("rotate90");
+  answer.classList.toggle("hidden");
+  liBefore.classList.toggle("new_before");
+  liBefore.classList.toggle("hide_before");
+}
+
+export function addEventToQuestionBtn() {
+  const questionsBtnsArr = document.querySelectorAll(".question_btn");
+  for (let el of questionsBtnsArr) {
+    el.addEventListener("click", () => showHideAnswer(el));
+  }
+}
