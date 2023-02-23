dc9d98929edbb4f49d2744aaf6df36ce934638f2
Halyna Stepanenko <105945642+galinavikst@users.noreply.github.com>
2023-02-23 20:37:32 +0100

    refactor: flip_cards with functionality

--- src/js/events.js
+++ src/js/events.js
@@ -1,21 +1,14 @@
-import { invert, invertBack } from "./invert-card.js";
 import { showHideAnswer } from "./questions.js";
 
 function addEvent(arr, event, func) {
   arr.forEach((el) => {
-    el.addEventListener(event, (ev) => {
-      // ev.stopPropagation();
+    el.addEventListener(event, () => {
       func(el);
     });
   });
 }
 
 export function addEvents() {
-  const cardsArr = document.querySelectorAll(".service_icon_item");
   const questionsItemArr = document.querySelectorAll(".question_item");
-
-  addEvent(cardsArr, "mouseover", invert);
-  addEvent(cardsArr, "mouseleave", invertBack);
-
   addEvent(questionsItemArr, "click", showHideAnswer);
 }
