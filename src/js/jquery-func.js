9d572693929b417fb0b0f79b8879de9051de5387
Halyna Stepanenko <105945642+galinavikst@users.noreply.github.com>
2023-02-25 10:00:59 +0100

    feat: add slick dots

--- src/js/jquery-func.js
+++ src/js/jquery-func.js
@@ -12,6 +12,8 @@ export function slider(elementClass, prevBtnClass, nextBtnClass) {
   $(document).ready(function () {
     $(elementClass).slick({
       arrows: false,
+      dots: true,
+      dotsClass: `slick-dots slick-dots-three`,
       infinite: false,
       speed: 300,
       slidesToShow: 4,
@@ -36,25 +38,18 @@ export function responsivSlider(elementClass) {
     if (windowWidth < 494) {
       // mobile devices
       $(elementClass).slick("slickSetOption", {
-        speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
       });
     } else if (windowWidth >= 680 && windowWidth < 1159) {
       // tablets
       $(elementClass).slick("slickSetOption", {
-        arrows: true,
-        infinite: false,
-        speed: 300,
         slidesToShow: 2,
         slidesToScroll: 2,
       });
     } else if (windowWidth >= 680 && windowWidth < 1159) {
       // desktops
       $(elementClass).slick("slickSetOption", {
-        arrows: true,
-        infinite: false,
-        speed: 300,
         slidesToShow: 3,
         slidesToScroll: 3,
       });
